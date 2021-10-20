f10832406e7e5682709c5a8671f0fe3a1ea4ae54
Massimiliano Riposati <38966126+maxriposati@users.noreply.github.com>
2021-10-20 22:51:12 +0200

    Delete otros_files directory

--- otros_files/forms.py
+++
@@ -1,8 +0,0 @@
-from wtforms import Form, StringField, SubmitField, validators
-
-class Formulario_Producto(Form):
-    id = StringField('Id', [validators.DataRequired()])
-    nombre = StringField('Nombre', [validators.DataRequired()])
-    precio = StringField('Precio', [validators.DataRequired()])
-    existencia = StringField('Existencia', [validators.DataRequired()])
-    enviar = SubmitField('Agregar producto')
